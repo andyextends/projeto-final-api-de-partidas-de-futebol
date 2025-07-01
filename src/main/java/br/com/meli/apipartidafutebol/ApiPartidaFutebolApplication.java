ca58e9e6b2d50afce33ddb2459e36cc672291069
andyextends <androamafreitas@gmail.com>
2025-07-01 10:34:39 -0300

    iniciando projeto adicionando arquivos de gitgnore ,pom .xml e src no proneto

---
+++ src/main/java/br/com/meli/apipartidafutebol/ApiPartidaFutebolApplication.java
@@ -0,0 +1,13 @@
+package br.com.meli.apipartidafutebol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPartidaFutebolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPartidaFutebolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/meli/apipartidafutebol/ApiPartidaFutebolApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meli.apipartidafutebol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPartidaFutebolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

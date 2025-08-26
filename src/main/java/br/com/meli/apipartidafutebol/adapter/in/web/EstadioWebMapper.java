216dd67052478618ae14663892f2a4dff68e3b61
andyextends <androamafreitas@gmail.com>
2025-08-26 00:34:18 -0300

    refactor(hexagonal): ajustar EstadioWebMapper para mapear corretamente entre DTO e entidade

--- src/main/java/br/com/meli/apipartidafutebol/adapter/in/web/EstadioWebMapper.java
+++ src/main/java/br/com/meli/apipartidafutebol/adapter/in/web/EstadioWebMapper.java
@@ -4,14 +4,21 @@ import br.com.meli.apipartidafutebol.dto.EstadioRequestDto;
 import br.com.meli.apipartidafutebol.dto.EstadioResponseDto;
 import br.com.meli.apipartidafutebol.model.Estadio;
 
-public final class EstadioWebMapper {
-    private EstadioWebMapper() {}
+public class EstadioWebMapper {
+    // DTO -> Entidade
     public static Estadio toDomain(EstadioRequestDto dto) {
-
-        return new Estadio(dto.getNome(), dto.getCidade(), dto.getCapacidade(), dto.getAtivo(), dto.getCep());
+        if (dto == null) return null;
+        return new Estadio(
+                dto.getNome(),
+                dto.getCidade(),
+                dto.getCapacidade(),
+                dto.getAtivo(),
+                dto.getCep()
+        );
     }
+    // Entidade -> DTO
     public static EstadioResponseDto toDto(Estadio estadio) {
-
-        return  new EstadioResponseDto(estadio);
+        if (estadio == null) return null;
+        return new EstadioResponseDto(estadio);
     }
 }
